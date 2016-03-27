cc5dcad7acba5e1a1c99dd128680f789d344a707
gifcompatible <okeydopeness@gmail.com>
2016-03-27 14:53:21 -0500

    Progress...I think

--- coffeescripts/application.coffee
+++ coffeescripts/application.coffee
@@ -3,39 +3,49 @@ myDataRef = new Firebase('https://q842lbplhsw.firebaseio-demo.com/')
 messages = []
 
 $(document).ready ->
+  user = new User()
 
-$('#messageInput').keypress (e) ->
-  if e.keyCode == 13
-    name = $('#nameInput').val()
-    text = $('#messageInput').val()
-    time = Date.now()
-    myDataRef.push name: name, text: text, time: time
-    $('#messageInput').val('')
+  $('#userInput').keypress (e) ->
+    if e.keyCode == 13
+      text = $('#nameInput').val()
+      user = new User(text)
+      $('#nameInput').hide()
 
+  $('#messageInput').keypress (e) ->
+    if e.keyCode == 13
 
-myDataRef.on 'child_added', (snapshot) ->
-  message = snapshot.val()
-  user = new User(message.name)
-  message = new Message(message.text, message.time, user)
-  messages.push message
-  messages.sort (a,b) ->
-    if a.timestamp > b.timestamp
-      return 1
-    else if b.timestamp > a.timestamp
-      return -1
-    else
-      return 0
-  displayChatMessage messages
+      text = $('#messageInput').val()
+      time = Date.now()
+      myDataRef.push name: user.fullName, text: text, time: time
+      $('#messageInput').val('')
 
 
-displayChatMessage = (messages) ->
-  $('#messagesDiv').text('')
-  messages.forEach (message) ->
-    $('<div/>').text(message.content).prepend(
-      $('<em/>').text("#{message.user.fullName}: ")
-    ).prepend(
-      $('<time/>').attr('datetime', message.timestamp.toISOString()).text( message.timestamp.toTimeString()).addClass('timeago')
-    ).appendTo($('#messagesDiv'))
-  $('#messagesDiv')[0].scrollTop =
-  $('#messagesDiv')[0].scrollHeight
-  $("time.timeago").timeago()
+  myDataRef.on 'child_added', (snapshot) ->
+    debugger
+    message = snapshot.val()
+    newUser = new User(message.name)
+    if newUser.fullName == user.fullName
+      newUser = User
+    message = new Message(message.text, message.time, newUser)
+    messages.push message
+    messages.sort (a,b) ->
+      if a.timestamp > b.timestamp
+        return 1
+      else if b.timestamp > a.timestamp
+        return -1
+      else
+        return 0
+    displayChatMessage messages
+
+
+  displayChatMessage = (messages) ->
+    $('#messagesDiv').text('')
+    messages.forEach (message) ->
+      $('<div/>').text(message.content).prepend(
+        $('<em/>').text(" #{message.user.fullName}: ")
+      ).prepend(
+        $('<time/>').attr('datetime', message.timestamp.toISOString()).text( message.timestamp.toTimeString()).addClass('timeago')
+      ).appendTo($('#messagesDiv'))
+    # $('#messagesDiv')[0].scrollTop =
+    # $('#messagesDiv')[0].scrollHeight
+    $("time.timeago").timeago()
--- javascripts/application.js
+++ javascripts/application.js
@@ -1,49 +1,59 @@
-var displayChatMessage, messages, myDataRef;
+var messages, myDataRef;
 
 myDataRef = new Firebase('https://q842lbplhsw.firebaseio-demo.com/');
 
 messages = [];
 
-$(document).ready(function() {});
-
-$('#messageInput').keypress(function(e) {
-  var name, text, time;
-  if (e.keyCode === 13) {
-    name = $('#nameInput').val();
-    text = $('#messageInput').val();
-    time = Date.now();
-    myDataRef.push({
-      name: name,
-      text: text,
-      time: time
-    });
-    return $('#messageInput').val('');
-  }
-});
-
-myDataRef.on('child_added', function(snapshot) {
-  var message, user;
-  message = snapshot.val();
-  user = new User(message.name);
-  message = new Message(message.text, message.time, user);
-  messages.push(message);
-  messages.sort(function(a, b) {
-    if (a.timestamp > b.timestamp) {
-      return 1;
-    } else if (b.timestamp > a.timestamp) {
-      return -1;
-    } else {
-      return 0;
+$(document).ready(function() {
+  var displayChatMessage, user;
+  user = new User();
+  $('#userInput').keypress(function(e) {
+    var text;
+    if (e.keyCode === 13) {
+      text = $('#nameInput').val();
+      user = new User(text);
+      return $('#nameInput').hide();
     }
   });
-  return displayChatMessage(messages);
-});
-
-displayChatMessage = function(messages) {
-  $('#messagesDiv').text('');
-  messages.forEach(function(message) {
-    return $('<div/>').text(message.content).prepend($('<em/>').text(message.user.fullName + ": ")).prepend($('<time/>').attr('datetime', message.timestamp.toISOString()).text(message.timestamp.toTimeString()).addClass('timeago')).appendTo($('#messagesDiv'));
+  $('#messageInput').keypress(function(e) {
+    var text, time;
+    if (e.keyCode === 13) {
+      text = $('#messageInput').val();
+      time = Date.now();
+      myDataRef.push({
+        name: user.fullName,
+        text: text,
+        time: time
+      });
+      return $('#messageInput').val('');
+    }
   });
-  $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
-  return $("time.timeago").timeago();
-};
+  myDataRef.on('child_added', function(snapshot) {
+    debugger;
+    var message, newUser;
+    message = snapshot.val();
+    newUser = new User(message.name);
+    if (newUser.fullName === user.fullName) {
+      newUser = User;
+    }
+    message = new Message(message.text, message.time, newUser);
+    messages.push(message);
+    messages.sort(function(a, b) {
+      if (a.timestamp > b.timestamp) {
+        return 1;
+      } else if (b.timestamp > a.timestamp) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+    return displayChatMessage(messages);
+  });
+  return displayChatMessage = function(messages) {
+    $('#messagesDiv').text('');
+    messages.forEach(function(message) {
+      return $('<div/>').text(message.content).prepend($('<em/>').text(" " + message.user.fullName + ": ")).prepend($('<time/>').attr('datetime', message.timestamp.toISOString()).text(message.timestamp.toTimeString()).addClass('timeago')).appendTo($('#messagesDiv'));
+    });
+    return $("time.timeago").timeago();
+  };
+});
